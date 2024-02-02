2284fb00d7e1c8a3434a7d0a33199abefc2c3f2f
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2024-02-02 09:12:10 +0100

    fix(app): Change route to save final list of links
    The route and the name of this file was harcoded, it change for dinamic name and route

--- app/controllers/merge_links.py
+++ app/controllers/merge_links.py
@@ -4,6 +4,8 @@ def merge_links(*archivos):
     enlaces_totales = set()
     current_dir = os.path.dirname(__file__)
     links_path = os.path.join(current_dir, "..", "links")
+    file_name = "total_links.txt"
+    file_path = os.path.join(links_path, file_name)
     
     if not os.path.exists(links_path):
             os.makedirs(links_path)
@@ -14,7 +16,7 @@ def merge_links(*archivos):
                 enlaces = f.readlines()
                 enlaces_totales.update(enlaces)
         enlaces_totales = list(enlaces_totales)
-        with open('enlaces_totales.txt', 'w') as f:
+        with open(file_path, 'w') as f:
             f.writelines(enlaces_totales)
     except Exception as e:
         print(f"Error al escribir enlaces: {e}")
