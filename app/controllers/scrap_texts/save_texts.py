a58f034227d22dac06d14cc74bd0463c3803f38c
ProKav <kamilo_vanegas@hotmail.com>
2024-02-06 12:50:02 +0100

    feat(app): Add cleaning features
    Before writes file, cleaning features added like removes header and footer, also, clean_filename function was extracted

--- app/controllers/scrap_texts/save_texts.py
+++ app/controllers/scrap_texts/save_texts.py
@@ -1,14 +1,9 @@
 import os
-from urllib.parse import urlparse
 from controllers.scrap_texts.clean_text import clean_text
+from controllers.scrap_texts.clean_filename import clean_filename
+from controllers.scrap_texts.remove_header_sspain import remove_header_sspain
+from controllers.scrap_texts.remove_footer_sspain import remove_footer_sspain
 
-def clean_filename(url):
-    parsed_url = urlparse(url)
-    host_name = parsed_url.netloc.replace(".", "_")
-    path_segments = parsed_url.path.strip("/").split("/")
-    path_name = "_".join(path_segments)
-
-    return f"{host_name}_{path_name}"
 
 def save_texts(text, url):
     current_dir = os.path.dirname(__file__)
@@ -16,14 +11,15 @@ def save_texts(text, url):
 
     clean_url = clean_filename(url)
     file_path = os.path.join(texts_path, f"{clean_url}.txt")
-    
+
     if not os.path.exists(texts_path):
         os.makedirs(texts_path)
-    
     try:
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write(text)
         clean_text(file_path)
+        remove_header_sspain(file_path)
+        remove_footer_sspain(file_path)
         print(f"Texto escrito en texts/{clean_url}.txt\n")
     except Exception as e:
         print(f"Error al escribir texto: {e}")
