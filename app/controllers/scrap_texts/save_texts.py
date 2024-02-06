77cf96a678e795af0b66986155d0f16484a1049e
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2024-02-06 21:15:54 +0100

    chore(app): Separate scraped files, write title and url in each document
    The URL is validated, depends of its domain, the file will be created in spain or global folder, also, each title and URL are written in the begin of file

--- app/controllers/scrap_texts/save_texts.py
+++ app/controllers/scrap_texts/save_texts.py
@@ -5,21 +5,37 @@ from controllers.scrap_texts.remove_header_sspain import remove_header_sspain
 from controllers.scrap_texts.remove_footer_sspain import remove_footer_sspain
 
 
-def save_texts(text, url):
+def save_texts(title, text, url):
     current_dir = os.path.dirname(__file__)
     texts_path = os.path.join(current_dir, "../..", "texts")
+    spain_path = os.path.join(texts_path, "spain")
+    global_path = os.path.join(texts_path, "global")
 
     clean_url = clean_filename(url)
-    file_path = os.path.join(texts_path, f"{clean_url}.txt")
 
     if not os.path.exists(texts_path):
         os.makedirs(texts_path)
+        os.makedirs(spain_path)
+        os.makedirs(global_path)
+        
+    if "scalian-spain" in url:
+        file_path = os.path.join(spain_path, f"{clean_url}.txt")
+    else:
+        file_path = os.path.join(global_path, f"{clean_url}.txt")
+        
     try:
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(text)
         clean_text(file_path)
         remove_header_sspain(file_path)
         remove_footer_sspain(file_path)
+        
+        with open(file_path, "r+", encoding="utf-8") as f:
+            content = f.read()
+            f.seek(0)
+            f.write(f"title: {title}\n")
+            f.write(f"url: {url}\n\n")
+            f.write(content)
         print(f"Texto escrito en texts/{clean_url}.txt\n")
     except Exception as e:
         print(f"Error al escribir texto: {e}")
